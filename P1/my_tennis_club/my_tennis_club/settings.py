03b81e713e0cf07dc8f619647f18287fad50ed42
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-09-21 09:28:11 -0600

    Practica 6

--- P1/my_tennis_club/my_tennis_club/settings.py
+++ P1/my_tennis_club/my_tennis_club/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-l%4jha*hmpb+&2940qjn^=%%1ok5oyvw@2h#8tp)q)5*o4hwlm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
