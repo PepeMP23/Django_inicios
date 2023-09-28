def07a4e2a19d594a4f48d54b7220751a5e52333
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-09-28 08:35:47 -0600

    Practica 7 Query Sets

--- P1/my_tennis_club/members/views.py
+++ P1/my_tennis_club/members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 def members(request): 
     mymembers = Member.objects.all().values()
@@ -24,8 +25,21 @@ def main(request):
     return HttpResponse(template.render())
 
 def testing(request):
+    #mydata = Member.objects.all()
+    #mydata = Member.objects.all().values()
+    #mydata = Member.objects.values_list('firstname')
+    #mydata = Member.objects.filter(firstname='Jose').values()
+    #mydata = Member.objects.filter(lastname='Refsnes', id=2).values()
+    #mydata = Member.objects.filter(firstname='Jose').values() | Member.objects.filter(firstname='Tobias').values()
+    #mydata = Member.objects.filter(Q(firstname='Jose') | Q(firstname='Tobias')).values()
+    #mydata = Member.objects.all().values().filter(firstname__startswith='J')
+    #mydata = Member.objects.filter(firstname__startswith='L').values()
+    #mydata = Member.objects.all().order_by('firstname').values()
+    #mydata = Member.objects.all().order_by('-firstname').values()
+    mydata = Member.objects.all().order_by('lastname', '-id').values()
+
     template = loader.get_template('template.html')
     context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],
+        'mymembers': mydata,
     }
     return HttpResponse(template.render(context, request))
