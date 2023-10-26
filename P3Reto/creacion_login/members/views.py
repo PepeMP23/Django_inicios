8a30d1db0361dea2c044aa2d36123343f5c917d2
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-10-26 09:45:06 -0600

    Login usuarios

---
+++ P3Reto/creacion_login/members/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+@login_required
+def index(request):
+    return render(request, 'all_members.html')
+
+def salir(request):
+    logout(request)
+    return redirect('/')
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+  
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def testing(request):
+    mydata = Member.objects.all().values()
+    template = loader.get_template('template.html')
+    context = {
+        'mymembers': mydata,
+    }
+    return HttpResponse(template.render(context, request))
