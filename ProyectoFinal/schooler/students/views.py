4814d819ecb613e86d285587d3397da08a1cec38
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-11-22 09:48:28 -0600

    Inicio Proyecto Final

---
+++ ProyectoFinal/schooler/students/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Student
+
+def students(request):
+    mystudents = Student.objects.all().values()
+    template = loader.get_template('all_students.html')
+    context = {
+        'mystudent': mystudents,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mystudent = Student.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mystudent': mystudent,
+    }
+    return HttpResponse(template.render(context, request))
